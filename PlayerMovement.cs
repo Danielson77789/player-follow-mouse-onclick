f2f5af841c6dc2e62af52f0580b93e3a02454fd9
Danielson77789 <59825907+Danielson77789@users.noreply.github.com>
2020-10-15 20:12:41 -0700

    Initial upload.

---
+++ PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] 
+    private float moveSpeed = 0;
+    [SerializeField] 
+    private Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+
+    //Called evenly even on frames
+    void FixedUpdate()
+    {
+        Move();
+        
+    }
+
+    void Move() {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+        );
+
+        Vector2 moveDirection = new Vector2(
+            mousePosition.x - transform.position.x * moveSpeed,
+            mousePosition.y - transform.position.y * moveSpeed
+        );
+
+        transform.up = direction;
+        if(Input.GetMouseButton(0)){
+            rb.AddForce(direction);
+        };
+    }
+
+
+}
